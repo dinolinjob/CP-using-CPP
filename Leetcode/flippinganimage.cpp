530385e9de922b5c559f8a67bbf1220da40b1422
dinolin yp job <dinolinypjob@karunya.edu.in>
2019-08-09 10:01:55 +0530

    Update flippinganimage.cpp

---
+++ Leetcode/flippinganimage.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& A) {
+        for(int i=0;i<A.size();i++){
+            reverse(A[i].begin(),A[i].end());
+        }
+        for(int i=0;i<A.size();i++){
+            for(int j=0;j<A.size();j++){
+                if(A[i][j]==0){
+                    A[i][j]=1;
+                }
+                else{
+                    A[i][j]=0;
+                }
+            }
+        }
+        return A;
+    }
+};

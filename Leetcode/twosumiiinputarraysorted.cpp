b66887782a229f9c3bc57953c0fb70d367427c7a
dinolin yp job <dinolinypjob@karunya.edu.in>
2019-09-02 14:37:38 +0530

    Create twosumiiinputarraysorted.cpp

---
+++ Leetcode/twosumiiinputarraysorted.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l=0,r=numbers.size()-1;
+        while(l<r){
+            if(numbers[l]+numbers[r]==target){
+                return vector<int> {l+1,r+1};
+            }
+            else if(numbers[l]+numbers[r]>target){
+                r--;
+            }
+            else{
+                l++;
+            }
+        }
+        return {};
+    }
+};
